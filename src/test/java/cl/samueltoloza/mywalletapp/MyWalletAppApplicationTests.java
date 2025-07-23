eac24f2031c456f06d97e38e1799bc035631e6b7
Samuel Toloza <tlz.system@gmail.com>
2025-07-22 20:49:12 -0400

    initial commit

---
+++ src/test/java/cl/samueltoloza/mywalletapp/MyWalletAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.samueltoloza.mywalletapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWalletAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
